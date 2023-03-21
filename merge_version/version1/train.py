a79faa3671bbccbe29176fa530226c8942f82702
junzhin <ningjunzhi85@gmail.com>
2023-03-21 20:12:53 +0800

    Update train.py

--- merge_version/version1/train.py
+++ merge_version/version1/train.py
@@ -49,13 +49,13 @@ if debug:
 default_layer_neurons = [128, 100, 110, 100, 10] # specify the number of layers and neurons in each layer
 default_layer_activation_funcs = [None, 'leakyrelu', 'leakyrelu', 'leakyrelu', 'softmax'] # None means linear,leakyrelu,relu,softmax,logistic
 default_learning_rate = 0.005 # learning rate for the optimizer
-default_epochs = 200 # number of training epochs
+default_epochs = 50 # number of training epochs
 default_dropout_prob = 1 # dropout probability that perserve the neuron
 assert 0 <= default_dropout_prob <= 1 # dropout probability must be between 0 and 1
 default_batch_size = 1000 # if batch_size is None, then no batch is used
 default_weight_decay = 0 # if weight_decay is None, then no weight decay is applied
 default_beta = [0.9, 0.999] # beta values for the adam optimizer
-default_size = 10000 # Size of training dataset, 50000 is the full dataset
+default_size = 50000 # Size of training dataset, 50000 is the full dataset
 default_batchnorm = True # True or False for batch normalization
 default_loss = 'CE' # 'CE' or 'MSE'
 default_optimizer = 'rmsprop'  # 'sgd' or 'adam', 'sgd_momentum' 'rmsprop'
